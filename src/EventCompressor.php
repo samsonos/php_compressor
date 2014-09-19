9bf8417f0c2e91c756692f2808215fa1d3c73bea
Vitaly Iegorov <egorov@samsonos.com>
2014-09-19 17:29:29 +0300

    Added event compressor class

---
+++ src/EventCompressor.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Created by Vitaly Iegorov <egorov@samsonos.com>
+ * on 19.09.2014 at 17:26
+ */
+ namespace samsonos\compressor;
+
+/**
+ * Compressor form SamsonPHP Event system
+ * @author Vitaly Egorov <egorov@samsonos.com>
+ * @copyright 2014 SamsonOS
+ */
+class EventCompressor 
+{
+    /** @var  array Collection of event subscriptions and handlers */
+    public $events;
+
+    protected function findAllDynamicCalls($code)
+    {
+
+    }
+
+    protected function findAllStaticCalls($code)
+    {
+
+    }
+
+    public function transform($input, & $output = '')
+    {
+
+    }
+}
+ 
