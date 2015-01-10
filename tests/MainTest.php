6059c491e0c739246f90e46e65717fbf80280b20
Vitaly Egorov <egorov@samsonos.com>
2015-01-10 14:32:31 +0200

    Added test and configs

---
+++ tests/MainTest.php
@@ -0,0 +1,24 @@
+<?php
+namespace tests;
+/**
+ * Created by Vitaly Iegorov <egorov@samsonos.com>
+ * on 04.08.14 at 16:42
+ */
+class MainTest extends \PHPUnit_Framework_TestCase
+{
+    public function testPushAndPop()
+    {
+        $stack = array();
+        $this->assertEquals(0, count($stack));
+
+        array_push($stack, 'foo');
+        $this->assertEquals('foo', $stack[count($stack)-1]);
+        $this->assertEquals(1, count($stack));
+
+		// Change to make push
+        $this->assertEquals('foo', array_pop($stack));
+        $this->assertEquals(0, count($stack));
+    }
+
+
+}
